9ba2fa2bb0d75e7ec914cae42c1fbecf51562f67
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2025-01-10 17:42:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,15 +24,13 @@ house_keeping() {
 
 # Function to change network settings
 function change_network {
-
     # Create a temporary file to store the input data
     TEMP_FILE=$(mktemp)
-    
-    
+
     # Display the menu with all fields in one form
     dialog --backtitle "Network Configuration" --title "Network Input" \
         --form "\nDialog Sample Label and Values" 25 60 16 \
-        "IP/CIDR:" 1 1 " . . . /" 1 25 25 30 \
+        "IP Address (CIDR):" 1 1 "10.1.1.1/24" 1 25 25 30 \
         "Gateway:" 2 1 "" 2 25 25 30 \
         "DNS (Comma separated):" 3 1 "" 3 25 25 30 \
         2>$TEMP_FILE
@@ -41,39 +39,57 @@ function change_network {
     if [[ $? -ne 0 ]]; then
         return 1  # Return to the previous menu
     else
-        # Read the values from the temporary file  
+        # Read the values from the temporary file
         readarray -t values < $TEMP_FILE
-        IP_ADDRESS="${values[0]}"
+        IP_CIDR="${values[0]}"
         GATEWAY="${values[1]}"
         DNS="${values[2]}"
 
-        if [ ! -z $GATEWAY ]; then
-            # Clean up the temporary file
-            rm -f $TEMP_FILE
+        # Remove the temporary file
+        rm -f $TEMP_FILE
 
-            # Display summary of network settings
-            dialog --title "Network Settings Summary" --msgbox \
-                "IP Address: $IP_ADDRESS\nNetmask: $NETMASK\nGateway: $GATEWAY\nDNS: $DNS" 10 50
- 
-            # Update the network configuration file
-            bash -c "cat > /tmp/network_file.tmp <<EOF
-            network:
-              version: 2
-              ethernets:
-                eth0:
-                  addresses: [$IP_ADDRESS/$NETMASK]
-                  gateway4: $GATEWAY
-                  nameservers:
-                    addresses: [$DNS]"
-            sudo cp /tmp/network_file.tmp /etc/netplan/01-netcfg.yaml
-            sudo netplan apply
+        # Clean up IP_CIDR to remove any trailing slashes
+        echo $IP_CIDR > /tmp/pre
+        IP_CIDR=$(echo $IP_CIDR | sed 's/\/$//')
+        echo $IP_CIDR > /tmp/pos
+
+        # Display summary of network settings
+        dialog --title "Network Settings Summary" --msgbox \
+            "IP Address: $IP_CIDR\nGateway: $GATEWAY\nDNS: $DNS" 10 50
+
+        # Validate the IP address format
+        if [[ $IP_CIDR =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
+            # Validate the gateway format
+            if [[ $GATEWAY =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                echo $IP_CIDR >> /tmp/pos
+                # Update the network configuration file
+                if [ -f /tmp/network_file.tmp ]; then
+                   rm /tmp/network_file.tmp
+                fi
+                bash -c "cat > /tmp/network_file.tmp <<EOF
+network:
+  version: 2
+  ethernets:
+    eth0:
+      addresses: [$IP_CIDR]
+      routes:
+       - to: default
+         via: $GATEWAY
+      nameservers:
+        addresses: [$DNS]
+EOF"
+                chmod 400 /tmp/network_file.tmp
+                sudo cp -p /tmp/network_file.tmp /etc/netplan/01-netcfg.yaml
+                sudo netplan apply
+            else
+                dialog --title "Network Settings Summary" --msgbox \
+                    "Invalid Gateway format. Please enter a valid IP address." 10 50
+            fi
         else
             dialog --title "Network Settings Summary" --msgbox \
-                "Please fulfill the gateway" 10 50
+                "Invalid IP address format. Please enter a valid IP address in CIDR notation." 10 50
         fi
     fi
-
-
 
     return 0  # Return success
 }

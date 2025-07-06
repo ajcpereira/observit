28510194c3644ef1a1873088bf34b2e81c1e5923
machado-nuno <nuno.machado@gmail.com>
2025-07-06 15:09:26 +0100

    added auth

--- functions/uds_server.py
+++ functions/uds_server.py
@@ -67,7 +67,8 @@ def uds_server_servicepools(**args):
                 "system": args['name'],
                 "resource_type": args['resources_types'],
                 "host": args['hostname'],
-                "pool_name": pool['name'],
+                "id": pool['id'],
+                "name": pool['name'],
                 #must check versio in the future
                 #"servicesTypeProvided": pool['info']['services_type_provided'],
                 "servicesTypeProvided": pool['info']['services_type_provided'] if 'services_type_provided' in pool['info'] else 'unknown',
@@ -79,7 +80,109 @@ def uds_server_servicepools(**args):
                 "cache_l2_srvs": pool['cache_l2_srvs'],
                 "user_services_count": pool['user_services_count'],
                 "user_services_in_preparation": pool['user_services_in_preparation'],
-                "usage_perc": float(pool['usage'].strip('%')),
+                "usage": float(pool['usage'].strip('%')),
+             },
+             "time": timestamp,
+         }
+        ]
+
+        logging.debug(f"Data to be sent to influxdb {influxdb_record}")
+
+        send_influxdb(
+            str(args['repository']),
+            str(args['repository_port']),
+            args['repository_api_key'],
+            args['repo_org'],
+            args['repo_bucket'],
+            influxdb_record
+        )
+        
+    #STEP 3: logout
+    #
+    #
+    request_url_path = rest_url_path + 'auth/logout'
+    response = http_client.get(request_url_path, headers={"X-Auth-Token": session_auth })
+
+    # Attempt to parse response as JSON
+    try:
+        response_json = response.json()
+        logging.debug(f"Logout response: {response_json}")
+    except Exception as e:
+        logging.error(f"Error parsing logout response: {e}")
+        return -1
+
+    http_client.close()
+
+    return 1
+
+
+
+def uds_server_authenticators(**args):
+
+    logging.debug(f"Arguments are url {args['uds_server_url']}, user {args['uds_server_user']} pass {args['uds_server_password_pwd64'][4]} and unsecure {args['uds_server_unsecured']}")
+
+    password = decode_base64(args['uds_server_password_pwd64'])
+
+    rest_url_path = 'uds/rest/'
+
+    # STEP 1: UDS server login
+    request_url_path = rest_url_path + 'auth/login'
+    request_body = {
+        "auth": args['uds_server_auth'],
+        "username": args['uds_server_user'],
+        "password": password
+    }
+
+    http_client = HttpConnect(base_url=args['uds_server_url'], unsecured=args['uds_server_unsecured'])
+    response = http_client.post(request_url_path, data=request_body)
+
+    # Attempt to parse response as JSON
+    try:
+        response_json = response.json()
+        logging.debug(f"Login response: {response_json}")
+    except Exception as e:
+        logging.error(f"Login response: {response.content} {response.headers['Date']}")
+        logging.error(f"Error parsing login response: {e}")
+        return -1
+
+    # Check login result
+    if response_json.get('result') != "ok":
+        logging.error(f"Login failed. Response: {response_json}")
+        return -1
+        
+    session_auth = response_json.get('token')
+    print(session_auth)
+
+    #STEP 2: retrieve authenticators
+    #
+    #
+    request_url_path = rest_url_path + 'authenticators/overview'
+    response = http_client.get(request_url_path, headers={"X-Auth-Token": session_auth })
+
+    # Attempt to parse response as JSON
+    try:
+        response_json = response.json()
+        date_str = response.headers['Date']
+        dt = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S GMT")
+        timestamp = dt.strftime("%Y-%m-%dT%H:%M:%S.00Z")
+        logging.debug(f"Authenticators pools request json response: {response_json}")
+    except Exception as e:
+        logging.error(f"Error parsing Authenticators response: {e}")
+        return -1
+
+    for auth in response_json:
+        
+        influxdb_record = [{
+             "measurement": "uds_server_authenticators",
+             "tags": {
+                "system": args['name'],
+                "resource_type": args['resources_types'],
+                "host": args['hostname'],
+                "id": auth['id'],
+                "name": auth['name'],
+              },
+             "fields": {
+                "users_count": auth['users_count'],
              },
              "time": timestamp,
          }
